a25b1060a14dee52c98a00c3c2bb7666d036c59d
ThatLukeDev <luke@l2j.co.uk>
2024-12-03 15:49:40 +0000

    visual example

--- kernel/main.c
+++ kernel/main.c
@@ -11,7 +11,6 @@
 #include "file.h"
 #include "graphics.h"
 #include "image.h"
-#include "audio.h"
 
 void main() {
 	initheap();
@@ -21,12 +20,19 @@ void main() {
 	initidt();
 	initclock();
 	initfs();
-	initaudio();
 
-	unsigned char* audio = malloc(10000);
-	for (int i = 0; i < 10000; i++) {
-		audio[i] = audio[i-1] * i * i * i * i * 2412412142 - 14921331321;
-	}
+	for (int i = 10; i < 100; i += 10)
+		drawoutline(i, i, 640 - i, 480 - i, 255, 255, 255);
 
-	playaudio(audio, 10000);
+	drawfill(100, 100, 640-100, 480-100, 255, 0, 0);
+
+	char* text = "Hello World!";
+	drawtext(text, 150, 150, 32, 0, 0, 255);
+
+	char* filename = "image.ppm";
+	unsigned char* ppmimage = fileRead(filename);
+	struct image img = loadPPM(ppmimage);
+	free(ppmimage);
+
+	drawimage(img, 128, 192);
 }
