af04b4e59f6ea25ba4f39da308d366ad6b482539
ThatLukeDev <luke@l2j.co.uk>
2024-12-18 15:39:59 +0000

    quick interactive experience

--- kernel/main.c
+++ kernel/main.c
@@ -21,5 +21,39 @@ void main() {
 	initclock();
 	initfs();
 
-	fileExec("helloworld.min");
+	printf("Welcome to Minimus! Type 'help' for a list of commands\n");
+	printf("(.min files are executable, but you can call them what you like)\n");
+	printf("(executing a non-executable file will crash)\n");
+
+	while (1) {
+		printf("> ");
+		char* input = gets();
+
+		if (!strcmp(input, "help")) {
+			printf("ls            Lists all files\n");
+			printf("[name]        Execute [name]\n");
+		}
+		else if (!strcmp(input, "ls")) {
+			char** files = fileList();
+
+			for (int i = 0; files[i] != 0; i++) {
+				printf("%s\n", files[i]);
+			}
+
+			free(files);
+		}
+		else {
+			char** files = fileList();
+
+			for (int i = 0; files[i] != 0; i++) {
+				if (!strcmp(files[i], input)) {
+					fileExec(input);
+				}
+			}
+
+			free(files);
+		}
+
+		free(input);
+	}
 }
