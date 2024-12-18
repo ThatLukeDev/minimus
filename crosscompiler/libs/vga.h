f91493e3435757eb40180ad78bf37e021513c3d0
ThatLukeDev <luke@l2j.co.uk>
2024-12-18 15:29:34 +0000

    fixed header

--- crosscompiler/libs/vga.h
+++ crosscompiler/libs/vga.h
@@ -1,5 +1,10 @@
+#ifndef VGA_INT
+#define VGA_INT
+
 extern void drawpixel(int _x, int _y, unsigned char r, unsigned char g, unsigned char b);
 
 extern void drawchar(int _x, int _y, unsigned char _char);
 
 extern unsigned char* font;
+
+#endif
