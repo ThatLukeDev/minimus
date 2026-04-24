0dbc2716c6771018340d5a0470fc95d548f25914
ThatLukeDev <noreply@noreply.co.uk>
2026-04-24 08:27:46 +0100

    sin, cos

--- os/dronegame.c
+++ os/dronegame.c
@@ -29,8 +29,47 @@ double sqrt(double x) {
 	return 1.0 / isqrt((float)x);
 }
 
+double pow(double x, int n) {
+	double result = 1;
+
+	for (int i = 0; i < n; i++)
+		result *= x;
+
+	return result;
+}
+
+int factorial(int x) {
+	if (x <= 1)
+		return 1;
+
+	return x * factorial(x - 1);
+}
+
+double sin(double x) {
+	double result = 0;
+
+	for (int i = 1; i < 20; i+=2) {
+		result += (double)pow(x, i) / factorial(i) * pow(-1, i/2);
+	}
+
+	return result;
+}
+
+double cos(double x) {
+	double result = 0;
+
+	for (int i = 0; i < 20; i+=2) {
+		result += (double)pow(x, i) / factorial(i) * pow(-1, i/2);
+	}
+
+	return result;
+}
+
 int main() {
-	printf("%d", (int)sqrt(25));
+	for (int i = 0; i < 10; i++) {
+		printf("sin(%d) -> %d\n", i, (int)(sin(i) * 10000));
+		printf("cos(%d) -> %d\n", i, (int)(cos(i) * 10000));
+	}
 	while (1);
 
 	char* args = *(char**)0x1000;
