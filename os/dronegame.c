7e8f918c2ed448c2063212ec33eab112bdb84b33
ThatLukeDev <noreply@noreply.co.uk>
2026-04-24 10:12:59 +0100

    basic trace

--- os/dronegame.c
+++ os/dronegame.c
@@ -11,6 +11,16 @@ struct vector3 {
 	double z;
 };
 
+struct sphere {
+	struct vector3 pos;
+	double r;
+};
+
+struct light {
+	struct vector3 pos;
+	double lum;
+};
+
 // fast inverse square root algorithm
 // https://en.wikipedia.org/wiki/Fast_inverse_square_root
 float isqrt(float x) {
@@ -63,6 +73,71 @@ double cos(double x) {
 	}
 
 	return result;
+}
+
+double clamp(double val, double low, double high) {
+	if (val < low)
+		return low;
+	if (val > high)
+		return high;
+	return val;
+}
+
+double intersects(struct vector3 origin, struct vector3 direction, struct sphere obj) {
+	struct vector3 SminusO = {
+		origin.x - obj.pos.x,
+		origin.y - obj.pos.y,
+		origin.z - obj.pos.z
+	};
+
+	double a = direction.x*direction.x + direction.y*direction.y + direction.z*direction.z;
+	double b = 2 * (direction.x * SminusO.x + direction.y * SminusO.y + direction.z * SminusO.z);
+	double c = SminusO.x * SminusO.x + SminusO.y * SminusO.y + SminusO.z * SminusO.z - obj.r;
+
+	double discriminant = b*b - 4*a*c;
+
+	if (discriminant < 0.0)
+		return -1;
+	else
+		return (-b - sqrt(discriminant)) / (2*a);
+}
+
+double trace(struct vector3 origin, struct vector3 direction, struct sphere* objs, struct light* lights) {
+	char foundObj = 0;
+	double lowestDist = 9999999999.0;
+
+	for (int i = 0; objs[i].r != 0; i++) {
+		double distance = intersects(origin, direction, objs[i]);
+
+		if (distance < lowestDist && distance > 0) {
+			lowestDist = distance;
+			foundObj = 1;
+		}
+	}
+
+	if (foundObj) {
+		struct vector3 pos = {
+			origin.x + direction.x * lowestDist,
+			origin.y + direction.y * lowestDist,
+			origin.z + direction.z * lowestDist,
+		};
+
+		double illumination = 0.0;
+
+		for (int i = 0; lights[i].lum != 0; i++) {
+			struct vector3 disp = {
+				origin.x - lights[i].pos.x,
+				origin.y - lights[i].pos.y,
+				origin.z - lights[i].pos.z,
+			};
+			double distance = sqrt(disp.x*disp.x + disp.y*disp.y + disp.z*disp.z);
+			illumination += lights[i].lum / (distance * distance);
+
+			return illumination;
+		}
+	}
+
+	return 0.0;
 }
 
 int main() {
